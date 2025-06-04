28c9589af369064c3bf4692dd2b2b65d9675f918
Lurked-Nero <neroisvogue@gmail.com>
2025-06-04 16:08:21 +0800

    fix: explicitly set favicon links via Head

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <Head>
-        {/* 多平台 favicon 設定 */}
-        <link rel="icon" href="/favicon_v4.ico" />
-        <link rel="shortcut icon" href="/favicon_v4.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_v4-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_v4-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_v4-180x180.png" />
+        <link rel="icon" href="/favicon_v4.ico?v=4" />
+        <link rel="shortcut icon" href="/favicon_v4.ico?v=4" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_v4-32x32.png?v=4" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_v4-16x16.png?v=4" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_v4-180x180.png?v=4" />
         <meta name="theme-color" content="#000000" />
       </Head>
       <body className="bg-black text-white">{children}</body>
